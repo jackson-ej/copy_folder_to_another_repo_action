64d1240ac0e4ec173e31d6f6778ceec0e5b04aa9
jackson-ej <81607137+jackson-ej@users.noreply.github.com>
2022-07-06 15:48:26 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,7 +46,6 @@ git add .
 if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MSG"
-  git pull
   echo "Pushing git commit"
   git push -u origin "HEAD:$OUTPUT_BRANCH"
 else
